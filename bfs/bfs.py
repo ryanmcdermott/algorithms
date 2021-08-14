f31a3c53db14bb4b8af7c9eec7567519f8bf3a61
Ryan McDermott <ryantmcdermott@gmail.com>
2021-08-14 13:14:07 -0700

    Fix BFS

--- bfs/bfs.py
+++ bfs/bfs.py
@@ -1,12 +1,4 @@
-from collections import defaultdict, deque
-
-
-class Graph:
-    def __init__(self):
-        self.graph = defaultdict(list)
-
-    def add_edge(self, u, v):
-        self.graph[u].append(v)
+from collections import deque
 
 
 def bfs(g, source):
@@ -18,7 +10,7 @@ def bfs(g, source):
         node = q.popleft()
         res.append(node)
 
-        for v in g.graph[node]:
+        for v in g[node]:
             if v not in visited:
                 visited.add(v)
                 q.append(v)
--- bfs/test_bfs.py
+++ bfs/test_bfs.py
@@ -1,16 +1,15 @@
 import unittest
-from bfs import bfs as traverse, Graph
+from bfs import bfs as traverse
 
 
 class BFSTest(unittest.TestCase):
     def test_connected_graph(self):
-        g = Graph()
-        g.add_edge(0, 1)
-        g.add_edge(0, 2)
-        g.add_edge(1, 2)
-        g.add_edge(2, 0)
-        g.add_edge(2, 3)
-        g.add_edge(3, 3)
+        g = {
+            0: [1, 2],
+            1: [2],
+            2: [0, 3],
+            3: []
+        }
 
         actual = traverse(g, 2)
         expected = [2, 0, 3, 1]
